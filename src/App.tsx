d8714b681f018bde9f296391486d3ef8a8fb5d53
Riuqlav <valquirsolza.vs@gmail.com>
2022-04-27 16:36:38 +0200

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import "./App.css";
-
-import React from "react";
 import { MultiChatWindow } from "react-chat-engine-advanced";
 
 const App = () => {
   return (
     <div>
-      <MultiChatWindow />
+      <MultiChatWindow style={{ height: "100vh" }} />
     </div>
   );
 };
