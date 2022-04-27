00c0937368f7d910f5f11db70fc274e7ac3c1d9c
Riuqlav <valquirsolza.vs@gmail.com>
2022-04-27 17:10:32 +0200

    Server and websocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { MultiChatWindow } from "react-chat-engine-advanced";
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+// Server-side rendering
+const projectID: string = "ae58ab19-ecc8-4e20-a30f-215820cf7538";
+const username: string = "VAL";
+const secret: string = "VAL321";
 
 const App = () => {
+  // Initialize the chat engine logic
+  const chatProps = useMultiChatLogic(projectID, username, secret);
   return (
     <div>
-      <MultiChatWindow style={{ height: "100vh" }} />
+      <MultiChatSocket {...chatProps} /> // Socket connection to update the
+      client side
+      <MultiChatWindow {...chatProps} style={{ height: "100vh" }} />
     </div>
   );
 };
